2d3bcc6aac2d06cc96103f21f0cd39db52b54c5b
soeguet <osman_s@live.de>
2024-04-21 22:35:10 +0200

    adjust api

--- main.go
+++ main.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 )
 
 type RequestValues struct {
-	Euro200 int `json:"euro200"`
-	Euro100 int `json:"euro100"`
-	Euro50  int `json:"euro50"`
-	Euro20  int `json:"euro20"`
-	Euro10  int `json:"euro10"`
-	Euro5   int `json:"euro5"`
-	Euro2   int `json:"euro2"`
-	Euro1   int `json:"euro1"`
-	Cent50  int `json:"cent50"`
-	Cent20  int `json:"cent20"`
-	Cent10  int `json:"cent10"`
-	Cent5   int `json:"cent5"`
-	Cent2   int `json:"cent2"`
-	Cent1   int `json:"cent1"`
+	Euro200 [5]int `json:"euro200"`
+	Euro100 [5]int `json:"euro100"`
+	Euro50  [5]int `json:"euro50"`
+	Euro20  [5]int `json:"euro20"`
+	Euro10  [5]int `json:"euro10"`
+	Euro5   [5]int `json:"euro5"`
+	Euro2   [5]int `json:"euro2"`
+	Euro1   [5]int `json:"euro1"`
+	Cent50  [5]int `json:"cent50"`
+	Cent20  [5]int `json:"cent20"`
+	Cent10  [5]int `json:"cent10"`
+	Cent5   [5]int `json:"cent5"`
+	Cent2   [5]int `json:"cent2"`
+	Cent1   [5]int `json:"cent1"`
 }
 
 type RequestValidation struct {
@@ -50,10 +51,13 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	fmt.Println("Received request")
+	fmt.Println(r.Body)
 	var payload RequestPayload
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&payload)
 	if err != nil {
+		fmt.Println("Error decoding payload")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -80,21 +84,29 @@ func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func sumArray(arr [5]int) int {
+	sum := 0
+	for _, v := range arr {
+		sum += v
+	}
+	return sum
+}
+
 func calculateTotalValue(request RequestPayload) ResponsePayload {
-	totalValue := float64(request.RequestValues.Euro200*200) +
-		float64(request.RequestValues.Euro100*100) +
-		float64(request.RequestValues.Euro50*50) +
-		float64(request.RequestValues.Euro20*20) +
-		float64(request.RequestValues.Euro10*10) +
-		float64(request.RequestValues.Euro5*5) +
-		float64(request.RequestValues.Euro2*2) +
-		float64(request.RequestValues.Euro1*1) +
-		float64(request.RequestValues.Cent50)*0.5 +
-		float64(request.RequestValues.Cent20)*0.2 +
-		float64(request.RequestValues.Cent10)*0.1 +
-		float64(request.RequestValues.Cent5)*0.05 +
-		float64(request.RequestValues.Cent2)*0.02 +
-		float64(request.RequestValues.Cent1)*0.01
+	totalValue := float64(sumArray(request.RequestValues.Euro200)*200) +
+		float64(sumArray(request.RequestValues.Euro100)*100) +
+		float64(sumArray(request.RequestValues.Euro50)*50) +
+		float64(sumArray(request.RequestValues.Euro20)*20) +
+		float64(sumArray(request.RequestValues.Euro10)*10) +
+		float64(sumArray(request.RequestValues.Euro5)*5) +
+		float64(sumArray(request.RequestValues.Euro2)*2) +
+		float64(sumArray(request.RequestValues.Euro1)*1) +
+		float64(sumArray(request.RequestValues.Cent50))*0.5 +
+		float64(sumArray(request.RequestValues.Cent20))*0.2 +
+		float64(sumArray(request.RequestValues.Cent10))*0.1 +
+		float64(sumArray(request.RequestValues.Cent5))*0.05 +
+		float64(sumArray(request.RequestValues.Cent2))*0.02 +
+		float64(sumArray(request.RequestValues.Cent1))*0.01
 
 	targetValueAsFloat, _ := strconv.ParseFloat(request.RequestValidation.TargetValue, 64)
 
